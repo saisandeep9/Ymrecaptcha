0507c63c6c1754afe0c4b2125e26b36ef576bca2
sai sandeep <mailtosaisandeep@gmail.com>
2021-06-09 10:28:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import ReCAPTCHA from "react-google-recaptcha";
 
 function App() {
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReCAPTCHA
+        style={{
+          display: "flex",
+          marginTop: "50px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        sitekey="6LfyLhsbAAAAANozFN9HmwZlhVWmyGiiDmF7_7gC"
+        onChange={onChange}
+      />
     </div>
   );
 }
